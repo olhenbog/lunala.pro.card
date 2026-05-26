0ef517e3c5969d9eec985802f49cf54f54e9955f
olhenbog <olhenbog@gmail.com>
2026-05-26 19:17:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
-const path = require('path');
-
-console.log('Рабочая директория:', __dirname);
-const files = fs.readdirSync(__dirname);
-console.log('Все файлы:', files);
-const pngFiles = files.filter(f => f.endsWith('.png'));
-console.log('PNG файлы:', pngFiles);
-    console.error('❌ Критическая ошибка:', err);
-    process.exit(1);
+console.log('📁 Содержимое корня:');
+fs.readdirSync('.').forEach(file => {
+    console.log(' -', file);
 });
